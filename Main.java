85670cba6e500326b1b736fcccdce21548ce03f7
Hisham Khalifa <h@k>
2013-01-20 18:18:06 +0000

    Initial commit. Stub main class and interfaces.

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * Assignment 3 of Programming in Java - Birkbeck, University of London
+ * Main class that manages and runs the contact manager application.
+ *
+ * Hisham Khalifa (MSc Computer Science 2012 - 2013, Full-Time)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Contact manager");
+    }
+}
